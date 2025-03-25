f194773b9ae1df70234fcef1e8f1535959b9303a
abdelkhaleksirinette6 <abdelkhaleksirinette6@gmail.com>
2025-03-25 17:26:17 +0100

    change

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  lastname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "user","recruteur"], default: "user" },
+  role: { 
+    type: String, 
+    enum: ["admin", "user","recruteur"],
+     default: "user" },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("user", userSchema);
